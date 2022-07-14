97e98b1c7453828da97944cd915525c8afc6316b
MichaelFranks27 <102259281+MichaelFranks27@users.noreply.github.com>
2022-07-14 15:19:31 -0700

    Add files via upload

--- Cal.py
+++ Cal.py
@@ -10,10 +10,10 @@ Cal = input("Input Your Choice ")
 if "1" in Cal:
  n1 = input("First Number ")
  n2 = input("Second Number ")
- print(str(int(n1) + int(n2))) #finally continue the rest for later
+ print(str(int(n1) + int(n2)))
 
 elif "2" in Cal:
- n1 = input("First Number ")  #nice fixed it
+ n1 = input("First Number ")
  n2 = input("Second Number ")
  print(str(int(n1) - int(n2)))
 
@@ -25,11 +25,11 @@ elif "3" in Cal:
 elif "4" in Cal:
  n1 = input("First Number ")
  n2 = input("Second Number ")
- print(str(int(n1) / int(n2))) #results can come out as a float
+ print(str(int(n1) / int(n2)))
 else:
  print("Error!" )
 
-#accidently used if instead of elif
+
 
 
 
